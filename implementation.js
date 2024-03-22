f7f9aa192bcaae10478826617dc707ea5b1ff175
Jordan Harband <ljharb@gmail.com>
2024-03-22 10:56:47 -0700

    [Deps] update `es-abstract`

--- implementation.js
+++ implementation.js
@@ -1,8 +1,8 @@
 'use strict';
 
 var ToObject = require('es-object-atoms/ToObject');
-var ToPropertyKey = require('es-abstract/2023/ToPropertyKey');
-var HasOwnProperty = require('es-abstract/2023/HasOwnProperty');
+var ToPropertyKey = require('es-abstract/2024/ToPropertyKey');
+var HasOwnProperty = require('es-abstract/2024/HasOwnProperty');
 
 module.exports = function hasOwn(O, P) {
 	var obj = ToObject(O);
